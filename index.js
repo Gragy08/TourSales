1b7e9173c5c5a7343a51d05b736e652098adaf79
Qui Tran <giaqui1712@gmail.com>
2025-03-26 16:46:27 +0700

    connect to database

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express')
-const path = require('path')
+const path = require('path');
+require('dotenv').config();
+//dotenv là một thư viện giúp chúng ta đọc file .env để lấy thông tin môi trường
+//Tạo một biến môi trường để khi đưa code lên gitlab không bị lộ thông tin của csdl
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE);
+
+
+//Define a schema
+const Tour = mongoose.model('Tour', {
+    name: String,
+    vehicle: String
+});
 
 const app = express()
 const port = 3000
@@ -18,12 +30,19 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/tours', (req, res) => {
+
+//await là chờ để lấy dữ liệu xong mới chạy xuống dòng code tiếp theo, dùng await phải có async
+app.get('/tours', async (req, res) => {
+    const tourList = await Tour.find({});
+
+    console.log(tourList);
+
     res.render("client/pages/tour-list", {
         pageTitle: "Danh sách tour",
+        tourList: tourList
     })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Website dang chay tren http://localhost:${port}`)
 })
