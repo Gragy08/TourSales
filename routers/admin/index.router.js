5ea0608b68e6d62122fb82a0d92e529e9f308d2f
Qui Tran <giaqui1712@gmail.com>
2025-03-29 16:40:35 +0700

    Tích hợp giao diện trang Quản lý Tour

--- routers/admin/index.router.js
+++ routers/admin/index.router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const accountRouters = require("./account.router");
 const dashboardRouters = require("./dashboard.router");
 const categoryRouters = require("./category.router");
+const tourRouters = require("./tour.router");
 
 //dùng use không dùng get để mấy router con lại không bị ảnh hưởng, các router còn con lại có thể sử dụng get, post, put, delete
 router.use('/account', accountRouters)
@@ -9,5 +10,7 @@ router.use('/account', accountRouters)
 router.use('/dashboard', dashboardRouters)
 
 router.use('/category', categoryRouters)
+
+router.use('/tour', tourRouters)
 
 module.exports = router;
