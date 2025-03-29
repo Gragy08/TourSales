b07cf5b09c9b10280f2c516ab7832100d990364a
Qui Tran <giaqui1712@gmail.com>
2025-03-29 16:03:51 +0700

    Tích hợp trang giao diện Tổng quan

--- routers/admin/index.router.js
+++ routers/admin/index.router.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const accountRouters = require("./account.router");
+const dashboardRouters = require("./dashboard.router");
 
 //dùng use không dùng get để mấy router con lại không bị ảnh hưởng, các router còn con lại có thể sử dụng get, post, put, delete
 router.use('/account', accountRouters)
+
+router.use('/dashboard', dashboardRouters)
 
 module.exports = router;
