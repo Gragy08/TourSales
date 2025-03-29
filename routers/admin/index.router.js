cf27218fae5594e8c8add37aa50cb5d8038f0bde
Qui Tran <giaqui1712@gmail.com>
2025-03-29 16:26:38 +0700

    Tích hợp trang Quản lý danh mục

--- routers/admin/index.router.js
+++ routers/admin/index.router.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const accountRouters = require("./account.router");
 const dashboardRouters = require("./dashboard.router");
+const categoryRouters = require("./category.router");
 
 //dùng use không dùng get để mấy router con lại không bị ảnh hưởng, các router còn con lại có thể sử dụng get, post, put, delete
 router.use('/account', accountRouters)
 
 router.use('/dashboard', dashboardRouters)
+
+router.use('/category', categoryRouters)
 
 module.exports = router;
