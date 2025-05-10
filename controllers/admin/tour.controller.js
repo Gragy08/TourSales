730f47b25152efd76627344982cb098dc7f79896
Qui Tran <giaqui1712@gmail.com>
2025-05-10 17:26:26 +0700

    Làm tính năng Phân trang và tìm kiếm cho Trang Tour

--- controllers/admin/tour.controller.js
+++ controllers/admin/tour.controller.js
@@ -187,12 +187,52 @@ module.exports.trash = async (req, res) => {
     const find = {
         deleted: true
     };
+
+    // Search
+    if (req.query.keyword) {
+        const keyword = req.query.keyword.trim();
+        find.$or = [
+            { name: { $regex: keyword, $options: "i" } }
+        ];
+    }
+    // End Search
+
+    // Pagination
+    // 3 items each page
+    const limitItems = 3;
+    // current page 
+    let page = 1;
+    if(req.query.page) {
+      // Lấy giá trị sau dấu "?" trên url
+      const currentPage =  parseInt(req.query.page);
+      if(currentPage > 0) {
+        page = currentPage;
+      }
+    }
+    const totalItems = await Tour.countDocuments(find);
+    const totalPages = Math.max(1, Math.ceil(totalItems / limitItems));
+    if(page > totalPages) {
+      page = totalPages;
+    }
+    const skip = (page - 1) * limitItems;
+
+    // Return the interface of the necessary variables
+    const pagination = {
+      skip: skip,
+      totalItems: totalItems,
+      totalPages: totalPages
+    }
+    // End Pagination
     
     const tourList = await Tour
     .find(find)
     .sort({
         deletedAt: "desc"
     })
+    // display limitItem items each page
+    .limit(limitItems)
+    // skip skip items
+    .skip(skip)
     
     for (const item of tourList) {
         if(item.createdBy) {
@@ -211,11 +251,13 @@ module.exports.trash = async (req, res) => {
 
         item.createdAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY");
         item.deletedAtFormat = moment(item.deletedAt).format("HH:mm - DD/MM/YYYY");
-    }    
+    }
 
     res.render("admin/pages/tour-trash", {
         pageTitle: "Thùng rác tour",
-        tourList: tourList    
+        tourList: tourList,
+        keyword: req.query.keyword || "",
+        pagination: pagination
     })
 }
 
