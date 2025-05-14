66ee48129cdac13bb9fe85ce7c05936ac81fb247
Qui Tran <giaqui1712@gmail.com>
2025-05-14 13:43:11 +0700

    Hiển thị data ra Top Header

--- routers/client/index.router.js
+++ routers/client/index.router.js
@@ -3,7 +3,12 @@ const homeRouters = require("./home.router");
 const tourRouters = require("./tour.router");
 const cartRouters = require("./cart.router");
 
-//dùng use không dùng get để mấy router con lại không bị ảnh hưởng, các router còn con lại có thể sử dụng get, post, put, delete
+const settingMiddleware = require("../../middlewares/client/setting.middleware");
+
+// tất cả các route ở client đều đi qua hàm websiteInfo
+router.use(settingMiddleware.websiteInfo);
+
+// dùng use không dùng get để mấy router con lại không bị ảnh hưởng, các router còn con lại có thể sử dụng get, post, put, delete
 router.use('/', homeRouters)
 router.use('/tours', tourRouters)
 router.use('/cart', cartRouters)
