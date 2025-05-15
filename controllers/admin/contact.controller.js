9d799f0a10e41f3373b8f9b91e540cf777d07583
Qui Tran <giaqui1712@gmail.com>
2025-05-15 19:20:40 +0700

    Tính năng Search và Phân trang trang Thông tin Liên hệ

--- controllers/admin/contact.controller.js
+++ controllers/admin/contact.controller.js
@@ -7,28 +7,66 @@ module.exports.list = async (req, res) => {
     };
 
     // Filtered by createAt
-        const dateFilter = {};
-    
-        if(req.query.startDate) {
-            const startDate = moment(req.query.startDate).startOf("date").toDate();
-            dateFilter.$gte = startDate;
-        }
-    
-        if(req.query.endDate) {
-            const endDate = moment(req.query.endDate).endOf("date").toDate();
-            dateFilter.$lte = endDate;
-        }
-    
-        if(Object.keys(dateFilter).length > 0) {
-            find.createdAt = dateFilter;
-        }
+    const dateFilter = {};
+
+    if(req.query.startDate) {
+        const startDate = moment(req.query.startDate).startOf("date").toDate();
+        dateFilter.$gte = startDate;
+    }
+
+    if(req.query.endDate) {
+        const endDate = moment(req.query.endDate).endOf("date").toDate();
+        dateFilter.$lte = endDate;
+    }
+
+    if(Object.keys(dateFilter).length > 0) {
+        find.createdAt = dateFilter;
+    }
     // End Filtered by createAt
+
+    // Search
+    if (req.query.keyword) {
+        const keyword = req.query.keyword.trim();
+        find.$or = [
+            { email: { $regex: keyword, $options: "i" } }
+        ];
+    }
+    // End Search
+
+    // Pagination
+    // 3 items each page
+    const limitItems = 3;
+    // current page 
+    let page = 1;
+    if(req.query.page) {
+        // Lấy giá trị sau dấu "?" trên url
+        const currentPage =  parseInt(req.query.page);
+        if(currentPage > 0) {
+        page = currentPage;
+        }
+    }
+    const totalItems = await Contact.countDocuments(find);
+    const totalPages = Math.max(1, Math.ceil(totalItems / limitItems));
+    if(page > totalPages) {
+        page = totalPages;
+    }
+    const skip = (page - 1) * limitItems;
+
+    // Return the interface of the necessary variables
+    const pagination = {
+        skip: skip,
+        totalItems: totalItems,
+        totalPages: totalPages
+    }
+    // End Pagination
 
     const contactList = await Contact
         .find(find)
         .sort({
             createAt: "desc"
-        });
+        })
+        .limit(limitItems)
+        .skip(skip)
 
     for (const item of contactList) {
         item.createdAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY");
@@ -36,7 +74,9 @@ module.exports.list = async (req, res) => {
 
     res.render("admin/pages/contact-list", {
         pageTitle: "Thông tin Liên hệ",
-        contactList: contactList
+        contactList: contactList,
+        keyword: req.query.keyword || "",
+        pagination: pagination
     })
 }
 
