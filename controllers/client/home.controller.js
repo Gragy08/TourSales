b377f30239f8e8e84faefba76da4c5f419fd6965
Qui Tran <giaqui1712@gmail.com>
2025-05-15 18:21:05 +0700

    Hiển thị data ra Section 4 (Trang chủ)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,6 @@
 const Tour = require("../../models/tour.model");
 const moment = require("moment");
+const categoryHelper = require("../../helpers/category.helper")
 
 module.exports.home = async (req, res) => {
   // Section 2
@@ -18,8 +19,33 @@ module.exports.home = async (req, res) => {
   }
   // End Section 2
 
+  // Section 4
+  const categoryIdSection4 = "6819eab8bf6ae1da307088fd"; // ID of Tour Trong Nuoc category
+  const listCategoryId = await categoryHelper.getAllSubcategoryIds(categoryIdSection4);
+
+  console.log(listCategoryId);
+
+  const tourListSection4 = await Tour
+    .find({
+      category: { $in: listCategoryId },
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      position: "desc"
+    })
+    .limit(8)
+
+  for(const item of tourListSection4) {
+    item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+  }
+
+  console.log(tourListSection4);
+  // End Section 4
+
   res.render("client/pages/home", {
     pageTitle: "Trang chủ",
-    tourListSection2: tourListSection2
+    tourListSection2: tourListSection2,
+    tourListSection4: tourListSection4
   })
 }
--- helpers/category.helper.js
+++ helpers/category.helper.js
@@ -1,3 +1,6 @@
+const Category = require("../models/category.model");
+
+// Get tree of category
 const buildCategoryTree = (categories, parentID = "") => {
     const tree = [];
 
@@ -21,3 +24,34 @@ const buildCategoryTree = (categories, parentID = "") => {
 
 // Didnt direct export bcs use recursive to buildCategoryTree func, then export this func
 module.exports.buildCategoryTree = buildCategoryTree;
+// End Get tree of category
+
+// Get all ids of category parent n child
+module.exports.getAllSubcategoryIds = async (parentId) => {
+  // Mảng lưu tất cả ID của danh mục (gồm danh mục cha và các danh mục con)
+  const result = [parentId];
+
+  // Hàm đệ quy để tìm các danh mục con
+  const findChildren = async (currentId) => {
+    // Tìm các danh mục con có parent = currentId, không bị xóa và đang hoạt động
+    const children = await Category
+      .find({
+        parent: currentId,
+        deleted: false,
+        status: "active"
+      });
+
+    // Duyệt qua từng danh mục con tìm được
+    for (const child of children) {
+      result.push(child.id); // Thêm ID vào danh sách kết quả
+      await findChildren(child.id); // Gọi đệ quy để tìm danh mục con của danh mục này
+    }
+  };
+
+  // Bắt đầu đệ quy từ danh mục gốc
+  await findChildren(parentId);
+
+  // Trả về toàn bộ danh sách ID đã thu thập được
+  return result;
+};
+// End Get all ids of category parent n child
