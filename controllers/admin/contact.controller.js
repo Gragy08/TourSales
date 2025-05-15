88f20da7a40a0c442bd9e3ed8ca27c6fb40c15e1
Qui Tran <giaqui1712@gmail.com>
2025-05-15 19:00:10 +0700

    Tính năng đăng ký email nhận tin (Trang chủ) bên BE

--- controllers/admin/contact.controller.js
+++ controllers/admin/contact.controller.js
@@ -1,5 +1,23 @@
+const Contact = require("../../models/contact.model");
+const moment = require("moment");
+
 module.exports.list = async (req, res) => {
+    const find = {
+        deleted: false
+    };
+
+    const contactList = await Contact
+        .find(find)
+        .sort({
+            createAt: "desc"
+        });
+
+    for (const item of contactList) {
+        item.createdAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY");
+    }
+
     res.render("admin/pages/contact-list", {
-        pageTitle: "Thông tin liên hệ"
+        pageTitle: "Thông tin liên hệ",
+        contactList: contactList
     })
 }
