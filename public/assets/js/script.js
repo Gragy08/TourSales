1a38151579419ade00d01acdeb05fa74f334d5b0
Qui Tran <giaqui1712@gmail.com>
2025-05-19 21:44:23 +0700

    Chỉ tính tiền những tour đã check

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -542,7 +542,8 @@ if(boxTourDetail) {
         quantityAdult: quantityAdult,
         quantityChildren: quantityChildren,
         quantityBaby: quantityBaby,
-        locationFrom: locationFrom
+        locationFrom: locationFrom,
+        checked: true
       };
 
       const cart = JSON.parse(localStorage.getItem("cart"));
@@ -600,7 +601,12 @@ const drawCart = () => {
               <button class="inner-delete" button-delete tour-id="${item.tourId}">
                 <i class="fa-solid fa-xmark"></i>
               </button>
-              <input class="inner-check" type="checkbox">
+              <input 
+                class="inner-check" 
+                type="checkbox" ${item.checked ? 'checked' : ''}
+                input-check
+                tour-id="${item.tourId}"
+              >
             </div>
             <div class="inner-product">
               <div class="inner-image">
@@ -698,7 +704,11 @@ const drawCart = () => {
 
         // Tính tổng tiền
         const subTotalPrice = data.cart.reduce((sum, item) => {
-          return sum + ((item.priceNewAdult * item.quantityAdult) + (item.priceNewChildren * item.quantityChildren) + (item.priceNewBaby * item.quantityBaby));
+          if(item.checked) {
+            return sum + ((item.priceNewAdult * item.quantityAdult) + (item.priceNewChildren * item.quantityChildren) + (item.priceNewBaby * item.quantityBaby));
+          } else {
+            return sum;
+          }
         }, 0);
         const discount = 0;
         const totalPrice = subTotalPrice - discount;
@@ -744,6 +754,22 @@ const drawCart = () => {
           })
         })
         // Hết Sự kiện xóa item
+
+        // Sự kiện check item
+        const listInputCheck = document.querySelectorAll("[input-check]");
+        listInputCheck.forEach(input => {
+          input.addEventListener("change", () => {
+            const checked = input.checked;
+            const tourId = input.getAttribute("tour-id");
+
+            const cart = JSON.parse(localStorage.getItem("cart"));
+            const itemUpdate = cart.find(item => item.tourId == tourId);
+            itemUpdate.checked = checked;
+            localStorage.setItem("cart", JSON.stringify(cart));
+            drawCart();
+          })
+        })
+        // Hết Sự kiện check item
       }
     })
 }
